8f980614832412bffdbaace955622b99134c93e3
Ne-x-tr-on <26nextron.dev@gmail.com>
2025-03-06 00:31:50 +0300

    Learn via Prcs

--- exercises/01_variables/variables2.rs
+++ exercises/01_variables/variables2.rs
@@ -1,10 +1,23 @@
-fn main() {
-    // TODO: Change the line below to fix the compiler error.
-    let x;
+// fn main() {
+//     // TODO: Change the line below to fix the compiler error.
+//     let x = 10;
 
-    if x == 10 {
+//     if x == 10 {
+//         println!("x is ten!");
+//     } else {
+//         println!("x is not ten!");
+//     }
+// }
+
+fn main(){
+
+    let  x =10;
+    if x == 10{
         println!("x is ten!");
-    } else {
+    }
+    else{
         println!("x is not ten!");
     }
+
+
 }
